36173ad96dc5dd28ad452b8b9ba32f2e8dedb237
ignaciopalmero <ignciopm@gmail.com>
2023-02-06 20:53:40 +0100

    starting main

---
+++ pys/functions.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+
+def clean_dataframe(dataframe):
+    
--- pys/main.py
+++ pys/main.py
@@ -1,13 +1,34 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
+
+
+abs_path = os.path.abspath('')
+
+csv = os.path.join(abs_path, "..", "resources", "books_1.Best_Books_Ever.csv")
+df = pd.read_csv(csv)
+
+# Initialization
+if 'df_user_input' not in st.session_state:
+    st.session_state['df_user_input'] = pd.DataFrame(columns=["title", "rating"])
 
 
 st.set_page_config(page_title="Book recommender", page_icon="📖")
 
-tab_model, tab_mapas = st.tabs(["Vote books","Recomendatios"])
+tab_vote, tab_recomendation = st.tabs(["Vote books","Recomendation"])
 
-with tab_model:
+with tab_vote:
 
 
-    city = st.selectbox('Select your city', options = ["Madrid", "Barcelona", "London"])
+    book = st.selectbox('Select the book', options = df["title"].values)
+
+    punctuation = st.slider("Select punctuation", 0, 10)
+
+    vote = st.button("Vote",help="")
+
+if vote:
+    st.session_state['df_user_input'] = st.session_state['df_user_input'].append({"title": book, "rating":punctuation}, ignore_index = True)
+
+
+st.write(st.session_state['df_user_input'])
